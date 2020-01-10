c9f00e262eac46e7d8645610b384864377ddb596
ChristiaanVDMerwe <37211962+ChristiaanVDMerwe@users.noreply.github.com>
2020-01-10 11:26:24 +0200

    Repo cleanup

--- src/main/java/org/apache/beam/examples/Ambrite.java
+++ src/main/java/org/apache/beam/examples/Ambrite.java
@@ -5,7 +5,6 @@
  */
 package org.apache.beam.examples;
 
-import org.apache.beam.examples.common.ExampleUtils;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.metrics.Counter;
